20ee635639c31e9dd66404a646c02ff00f95a722
iabarcah <ignacio.abarca@alumnos.uv.cl>
2021-07-01 18:08:18 -0400

    Update main.cc

--- code/src/main.cc
+++ code/src/main.cc
@@ -105,11 +105,12 @@ int main(int argc, char *argv[])
 	
 	//Cada procesa realiza su suma parcial
 	float sumaParcial = 0.0;
-	mpi::scatter(world,numbersToSum,sumaParcial,ntotalByProc,0);
+	
 	for(size_t idx = 0; idx < ntotalByProc; idx++){
 		sumaParcial += std::sin((numbersToSum[idx]))*std::cos((numbersToSum[idx]));
 	}
-	
+	//mpi::scatter(world, dataToShare, dataByProc, ntotalByProc, senderRank);
+	mpi::scatter(world,numbersToSum,sumaParcial,ntotalByProc,senderRank);
 	// ###############Completar código############
 	//Los procesos envian sus resultados parciales para que el proceso 0 haga la reducción
 	float sumaParalela = 0.0;
